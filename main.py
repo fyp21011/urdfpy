d5aea2455f861b90fa5b6ff6c015548d6320bd9c
EE-LiuYunhao <liuyunhao1578@gmail.com>
2021-11-07 22:04:11 +0800

    Move vis-urdf to main

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import argparse
+
 from urdfpy import URDF
 
-def main():
-    robot = URDF.load("tests/data/ur5/ur5.urdf")
-    robot.show()
+def main(path:str, animate:bool=True):
+    robot = URDF.load(path)
+    if animate:
+        robot.animate()
+    else:
+        robot.show()
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str,
+                        help='Path to URDF file that describes the robot')
+    parser.add_argument('-a', action='store_true',
+                        help='Visualize robot articulation')
+    parser.add_argument('-c', action='store_true',
+                        help='Use collision geometry')
+
+    args = parser.parse_args()
+    main(args.path, args.a)
