b7b11f03cd145560f7ac6dcf4f32acb871fa4972
EE-LiuYunhao <liuyunhao1578@gmail.com>
2021-11-24 20:14:07 +0800

    remove trimesh from unit tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,14 +1,15 @@
 import numpy as np
 import trimesh.transformations as tfs
+from open3d import geometry as o3dgm
 
 from urdfpy import (rpy_to_matrix, matrix_to_rpy, matrix_to_xyz_rpy,
                     xyz_rpy_to_matrix)
 
 
 def test_rpy_to_matrix():
-    xr45 = tfs.rotation_matrix(np.pi / 2, [1, 0, 0])[:3,:3]
-    yr45 = tfs.rotation_matrix(np.pi / 2, [0, 1, 0])[:3,:3]
-    zr45 = tfs.rotation_matrix(np.pi / 2, [0, 0, 1])[:3,:3]
+    xr45 = o3dgm.get_rotation_matrix_from_axis_angle([np.pi / 2, 0, 0])
+    yr45 = o3dgm.get_rotation_matrix_from_axis_angle([0, np.pi / 2, 0])
+    zr45 = o3dgm.get_rotation_matrix_from_axis_angle([0, 0, np.pi / 2])
     c = zr45.dot(yr45.dot(xr45))
 
     assert np.allclose(rpy_to_matrix([np.pi / 2, 0, 0]), xr45)
@@ -18,9 +19,9 @@ def test_rpy_to_matrix():
 
 
 def test_matrix_to_rpy():
-    xr45 = tfs.rotation_matrix(np.pi / 2, [1, 0, 0])[:3,:3]
-    yr45 = tfs.rotation_matrix(np.pi / 2, [0, 1, 0])[:3,:3]
-    zr45 = tfs.rotation_matrix(np.pi / 2, [0, 0, 1])[:3,:3]
+    xr45 = o3dgm.get_rotation_matrix_from_axis_angle([np.pi / 2, 0, 0])
+    yr45 = o3dgm.get_rotation_matrix_from_axis_angle([0, np.pi / 2, 0])
+    zr45 = o3dgm.get_rotation_matrix_from_axis_angle([0, 0, np.pi / 2])
 
     assert np.allclose([np.pi / 2, 0, 0], matrix_to_rpy(xr45))
     assert np.allclose([0, np.pi / 2, 0], matrix_to_rpy(yr45))
@@ -28,9 +29,12 @@ def test_matrix_to_rpy():
 
 
 def test_matrix_to_xyz_rpy():
-    xr45 = tfs.rotation_matrix(np.pi / 2, [1, 0, 0])
-    yr45 = tfs.rotation_matrix(np.pi / 2, [0, 1, 0])
-    zr45 = tfs.rotation_matrix(np.pi / 2, [0, 0, 1])
+    xr45 = np.eye(4)
+    yr45 = np.eye(4)
+    zr45 = np.eye(4)
+    xr45[:3, :3] = o3dgm.get_rotation_matrix_from_axis_angle([np.pi / 2, 0, 0])
+    yr45[:3, :3] = o3dgm.get_rotation_matrix_from_axis_angle([0, np.pi / 2, 0])
+    zr45[:3, :3] = o3dgm.get_rotation_matrix_from_axis_angle([0, 0, np.pi / 2])
 
     xr45[:3,3] = np.array([1,2,3])
     yr45[:3,3] = np.array([2,3,1])
@@ -42,9 +46,12 @@ def test_matrix_to_xyz_rpy():
 
 
 def test_xyz_rpy_to_matrix():
-    xr45 = tfs.rotation_matrix(np.pi / 2, [1, 0, 0])
-    yr45 = tfs.rotation_matrix(np.pi / 2, [0, 1, 0])
-    zr45 = tfs.rotation_matrix(np.pi / 2, [0, 0, 1])
+    xr45 = np.eye(4)
+    yr45 = np.eye(4)
+    zr45 = np.eye(4)
+    xr45[:3, :3] = o3dgm.get_rotation_matrix_from_axis_angle([np.pi / 2, 0, 0])
+    yr45[:3, :3] = o3dgm.get_rotation_matrix_from_axis_angle([0, np.pi / 2, 0])
+    zr45[:3, :3] = o3dgm.get_rotation_matrix_from_axis_angle([0, 0, np.pi / 2])
 
     xr45[:3,3] = np.array([1,2,3])
     yr45[:3,3] = np.array([2,3,1])
